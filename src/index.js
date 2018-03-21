fd4729d97e3eee3e3fa2821a488edb505cf99876
Pratibha Rawat <prawat@amazon.com>
2018-03-20 17:29:19 -0700

    Redux/ReactNative

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyAakGnJ6c10UCALi1PNYABYm1wyWJKl5xk';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state = {
+        videos: [],
+        selectedVideo: null
+      };
+
+    YTSearch({key: API_KEY, term: 'bollywood'}, (videos) =>   {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail  video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
